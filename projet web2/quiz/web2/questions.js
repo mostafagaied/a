6f171a810db897e870c949f42a77b3af8e1ad0c1
mostafagaied <mostafagayed018@gmail.com>
2024-12-15 01:09:20 +0100

    Add files via upload

---
+++ projet web2/quiz/web2/questions.js
@@ -0,0 +1,116 @@
+let questions = [
+    {
+       numb: 1,
+       question: "What does HTML stand for?",
+       answer: "C. Hyper Text Markup Language",
+       options: [
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language",
+
+       ]     
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+          "A. Cascading Style Sheet",
+          "B. Cute Style Sheet",
+          "C. Computer Style Sheet",
+          "D. Codehal Style Sheet",
+        ]     
+     },
+     {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+          "A. Hypertext Preprocessor",
+          "B. Hometext Programming",
+          "C. Hypertext Preprogramming",
+          "D. Programming Hypertext Preprocessor",
+        ]     
+     },
+     {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+         "A. Structured Query Language",
+         "B. Structured Query Language",
+         "C. Structured Query Language",
+         "D. Structured Query Language",
+        ]     
+     },
+     {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+         "A. Excellent Multiple Language",
+         "B. Explore Multiple Language",
+         "C. Extra Markup Language",
+         "D. Extensible Markup Language",
+        ]     
+     },
+] 
+let questionsf = [
+   {
+      numb: 1,
+      question: "222222?",
+      answer: "C. mostafa",
+      options: [
+       "A. messi",
+       "B. ronaldo",
+       "C. mostafa",
+       "D. iniesta",
+
+      ]     
+   },
+   {
+       numb: 2,
+       question: "how many ?",
+       answer: "A. 8",
+       options: [
+         "A. 8",
+         "B. 6",
+         "C. 1",
+         "D. 5",
+       ]     
+    },
+    {
+       numb: 3,
+       question: "What does PHP stand for?",
+       answer: "A. Hypertext Preprocessor",
+       options: [
+         "A. Hypertext Preprocessor",
+         "B. Hometext Programming",
+         "C. Hypertext Preprogramming",
+         "D. Programming Hypertext Preprocessor",
+       ]     
+    },
+    {
+       numb: 4,
+       question: "What does SQL stand for?",
+       answer: "D. Structured Query Language",
+       options: [
+        "A. Structured Query Language",
+        "B. Structured Query Language",
+        "C. Structured Query Language",
+        "D. Structured Query Language",
+       ]     
+    },
+    {
+       numb: 5,
+       question: "What does XML stand for?",
+       answer: "D. Extensible Markup Language",
+       options: [
+        "A. Excellent Multiple Language",
+        "B. Explore Multiple Language",
+        "C. Extra Markup Language",
+        "D. Extensible Markup Language",
+       ]     
+    },
+] 
